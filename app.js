556ab6a3156909247a0dbad744582b3c7832e7f5
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-05-25 09:18:16 +0000

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+console.clear();
 require("dotenv");
 require("express-async-errors");
 const express = require("express");
-const connectDB = require("./db/connect");
-
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 const app = express();
+
+// middlewares
+app.use(notFoundMiddleware, errorHandlerMiddleware);
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-	await connectDB(process.env.);
-	app.listen(port, () => {
-		console.log(`server is running on port ${port}`);
-	});
+	try {
+		app.listen(port, () => {
+			console.log(`server is running on port ${port}`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 start();
